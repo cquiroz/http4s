221dbd753ef036c1b034ded6dacb4674ac8a2262
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2017-06-23 23:09:07 -0400

    Use dependencies with the scalajs version where needed

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -5,6 +5,8 @@ import sbt._, Keys._
 import com.typesafe.tools.mima.plugin.MimaPlugin, MimaPlugin.autoImport._
 import sbtrelease._
 import sbtrelease.ReleasePlugin.autoImport._
+import org.scalajs.sbtplugin.ScalaJSPlugin
+import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 import scala.util.Properties.envOrNone
 import verizon.build.RigPlugin, RigPlugin._
 
@@ -105,12 +107,12 @@ object Http4sPlugin extends AutoPlugin {
   lazy val circeParser                      = "io.circe"               %% "circe-parser"              % circeJawn.revision
   lazy val cryptobits                       = "org.reactormonk"        %% "cryptobits"                % "1.1"
   lazy val discipline                       = "org.typelevel"          %% "discipline"                % "0.7.3"
-  lazy val fs2Cats                          = "co.fs2"                 %% "fs2-cats"                  % "0.3.0"
-  lazy val fs2Io                            = "co.fs2"                 %% "fs2-io"                    % "0.9.5"
+  lazy val fs2Cats                          = Def.setting("co.fs2"                 %%% "fs2-cats"                  % "0.3.0")
+  lazy val fs2Io                            = Def.setting("co.fs2"                 %%% "fs2-io"                    % "0.9.5")
   lazy val fs2ReactiveStreams               = "com.github.zainab-ali"  %% "fs2-reactive-streams"      % "0.1.0"
   lazy val gatlingTest                      = "io.gatling"             %  "gatling-test-framework"    % "2.2.5"
   lazy val gatlingHighCharts                = "io.gatling.highcharts"  %  "gatling-charts-highcharts" % gatlingTest.revision
-  lazy val http4sWebsocket                  = "org.http4s"             %% "http4s-websocket"          % "0.2.0"
+  lazy val http4sWebsocket                  = Def.setting("org.http4s"             %%% "http4s-websocket"          % "0")
   lazy val javaxServletApi                  = "javax.servlet"          %  "javax.servlet-api"         % "3.1.0"
   lazy val jawnJson4s                       = "org.spire-math"         %% "jawn-json4s"               % "0.10.4"
   lazy val jawnFs2                          = "org.http4s"             %% "jawn-fs2"                  % "0.10.1"
@@ -120,9 +122,9 @@ object Http4sPlugin extends AutoPlugin {
   lazy val json4sJackson                    = "org.json4s"             %% "json4s-jackson"            % json4sCore.revision
   lazy val json4sNative                     = "org.json4s"             %% "json4s-native"             % json4sCore.revision
   lazy val jspApi                           = "javax.servlet.jsp"      %  "javax.servlet.jsp-api"     % "2.3.1" // YourKit hack
-  lazy val log4s                            = "org.log4s"              %% "log4s"                     % "1.3.6"
+  lazy val log4s                            = Def.setting("org.log4s"              %%% "log4s"                     % "1.3.6")
   lazy val logbackClassic                   = "ch.qos.logback"         %  "logback-classic"           % "1.2.3"
-  lazy val macroCompat                      = "org.typelevel"          %% "macro-compat"              % "1.1.1"
+  lazy val macroCompat                      = Def.setting("org.typelevel"          %%% "macro-compat"              % "1.1.1")
   lazy val metricsCore                      = "io.dropwizard.metrics"  %  "metrics-core"              % "3.2.3"
   lazy val metricsJson                      = "io.dropwizard.metrics"  %  "metrics-json"              % metricsCore.revision
   lazy val quasiquotes                      = "org.scalamacros"        %% "quasiquotes"               % "2.1.0"
@@ -130,7 +132,7 @@ object Http4sPlugin extends AutoPlugin {
   def scalaCompiler(so: String, sv: String) = so                       %  "scala-compiler"            % sv
   def scalaReflect(so: String, sv: String)  = so                       %  "scala-reflect"             % sv
   lazy val scalaXml                         = "org.scala-lang.modules" %% "scala-xml"                 % "1.0.6"
-  lazy val scodecBits                       = "org.scodec"             %% "scodec-bits"               % "1.1.4"
+  lazy val scodecBits                       = Def.setting("org.scodec"             %%% "scodec-bits"               % "1.1.4")
   lazy val specs2Core                       = "org.specs2"             %% "specs2-core"               % "3.8.6"
   lazy val specs2MatcherExtra               = "org.specs2"             %% "specs2-matcher-extra"      % specs2Core.revision
   lazy val specs2Scalacheck                 = "org.specs2"             %% "specs2-scalacheck"         % specs2Core.revision
